51a038df0661acf0df7e189062427a6cafa0db5b
kevinberg3 <kevinberg3@gmail.com>
2013-07-17 17:53:47 -0400

    Writing tests for active navbar element

--- test/integration/project_flows_test.rb
+++ test/integration/project_flows_test.rb
@@ -47,6 +47,13 @@ class ProjectFlowsTest < ActionDispatch::IntegrationTest
     assert_equal projects_path, current_path
     # Assert the projects nav element is active
     assert_equal "Projects", find('.navbar ul li.active a').text
+
+    # Create a project to visit its show page at the end of the test
+    project1 = FactoryGirl.create(:project, :title => "Project 1")
+    
+    # On a project's show page, the Projects nav element should still be active
+    click_link 'Project 1'
+    assert_equal "Projects", find('.navbar ul li.active a').text
     end
 
 
