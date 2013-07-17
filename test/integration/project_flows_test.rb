6b78a71d8085ee0ce7c00615b17cd0f73b29fa1f
kevinberg3 <kevinberg3@gmail.com>
2013-07-17 14:15:06 -0400

    Writing tests for browsing projects

---
+++ test/integration/project_flows_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class ProjectFlowsTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "browsing projects" do
+    #create three projects 
+    project1 = FactoryGirl.create(:project, title: "Project 1")
+    project2 = FactoryGirl.create(:project, title: "Project 2")
+    project3 = FactoryGirl.create(:project, title: "Project 3")
+
+    # Go to the page with the projects
+    visit "/projects"
+
+    # Assert this page we're on is the page with the projects
+    assert_equal projects_path, current_path
+
+    # Assert this page has the words 'Listing Projects'
+    assert page.has_content?('Listing projects')
+
+    # Assert this page has these words as well
+    assert page.has_content?('Project 1')
+    assert page.has_content?('Project 2')
+    assert page.has_content?('Project 3')
+  end
+end
