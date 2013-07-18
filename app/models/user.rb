6a5a4de9fc9b3da1ee729f0245f343f54cb43159
kevinberg3 <kevinberg3@gmail.com>
2013-07-17 23:41:36 -0400

    Generating pledge model and adding associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
   has_many :projects
+  has_many :pledges
 
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
